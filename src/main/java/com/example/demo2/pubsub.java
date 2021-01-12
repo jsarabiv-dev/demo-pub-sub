aedc71b257ad3aae4cf707f31c7b05095c9420d2
jsarabiv-dev <jsarabiv.dev@gmail.com>
2021-01-12 12:19:57 -0300

    First commit pub/sub jsarabiv

---
+++ src/main/java/com/example/demo2/pubsub.java
@@ -0,0 +1,107 @@
+package com.example.demo2;
+
+import com.google.cloud.spring.pubsub.core.PubSubOperations;
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import com.google.cloud.spring.pubsub.integration.AckMode;
+import com.google.cloud.spring.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+import com.google.cloud.spring.pubsub.integration.outbound.PubSubMessageHandler;
+import com.google.cloud.spring.pubsub.support.BasicAcknowledgeablePubsubMessage;
+import com.google.cloud.spring.pubsub.support.GcpPubSubHeaders;
+import com.google.cloud.spring.pubsub.support.converter.SimplePubSubMessageConverter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+@Slf4j
+@EnableIntegration
+public class pubsub {
+
+    @Autowired
+    ReportPubSubOutboundGateway messageGateway;
+
+
+    /*
+    @Bean
+    public PubSubInboundChannelAdapter messageChannelAdapter(
+            @Qualifier("pubsubInputChannel") MessageChannel inputChannel, PubSubTemplate pubSubTemplate) {
+
+        pubSubTemplate.setMessageConverter(new SimplePubSubMessageConverter(StandardCharsets.UTF_8));
+        PubSubInboundChannelAdapter adapter =
+                new PubSubInboundChannelAdapter(pubSubTemplate, "suscripcion-entrada");
+        adapter.setOutputChannel(inputChannel);
+        adapter.setAckMode(AckMode.MANUAL);
+        adapter.setPayloadType(String.class);
+        return adapter;
+    }
+    */
+
+    @Bean
+    public PubSubInboundChannelAdapter messageChannelAdapter(
+            @Qualifier("pubsubInputChannel") MessageChannel inputChannel, PubSubTemplate pubSubTemplate) {
+        PubSubInboundChannelAdapter adapter =
+                new PubSubInboundChannelAdapter(pubSubTemplate, "suscripcion-entrada");
+        adapter.setOutputChannel(inputChannel);
+        adapter.setAckMode(AckMode.MANUAL);
+        return adapter;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "pubsubInputChannel")
+    public MessageHandler messageReceiver() {
+        return message -> {
+            log.info("Message arrived! Payload: " + new String((byte[]) message.getPayload()));
+            sendMessagePubSub(new String((byte[]) message.getPayload()));
+            BasicAcknowledgeablePubsubMessage originalMessage =
+                    message
+                            .getHeaders()
+                            .get(GcpPubSubHeaders.ORIGINAL_MESSAGE, BasicAcknowledgeablePubsubMessage.class);
+            originalMessage.ack();
+        };
+    }
+
+    /*
+    @ServiceActivator(inputChannel = "pubsubInputChannel")
+    public void receive(
+            String payload,
+            @Header(GcpPubSubHeaders.ORIGINAL_MESSAGE) BasicAcknowledgeablePubsubMessage originalMessage) {
+        log.info("This message arrived => {}", payload);
+        originalMessage.ack();
+    }
+    */
+
+    @Bean
+    public MessageChannel pubsubInputChannel() {
+        return new DirectChannel();
+    }
+
+    public void sendMessagePubSub(String json) {
+        messageGateway.sendToPubsub(json);
+    }
+
+    @MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
+    public interface ReportPubSubOutboundGateway {
+        void sendToPubsub(String text);
+    }
+
+
+    @Bean
+    @ServiceActivator(inputChannel = "pubsubOutputChannel")
+    public MessageHandler messageSender(PubSubOperations pubsubTemplate) {
+        return new PubSubMessageHandler(pubsubTemplate, "tema-salida");
+    }
+
+
+}
